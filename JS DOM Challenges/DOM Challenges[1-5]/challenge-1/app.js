ffad2247394877fb7b9d3ccc5afc3a70fdc49c82
Nidhibhide <nidhibhide144@gmail.com>
2025-03-02 22:49:46 +0530

    Challenge 1 is finished

--- JS DOM Challenges/DOM Challenges[1-5]/challenge-1/app.js
+++ JS DOM Challenges/DOM Challenges[1-5]/challenge-1/app.js
@@ -1,3 +1,24 @@
-/**
- * Write your challenge solution here
- */
+const statusText = document.getElementById("status");
+const btn = document.getElementById("toggleButton");
+const bulb = document.getElementById("bulb");
+const body = document.getElementById("body").classList;
+
+btn.addEventListener("click", () => {
+  if (statusText.innerText === "Status: Off") {
+    body.remove("light-mode");
+    body.add("dark-mode");
+
+    statusText.innerText = "Status: On";
+    btn.innerText = "Turn Off";
+
+    bulb.classList.remove("off");
+  } else {
+    body.remove("dark-mode");
+    body.add("light-mode");
+
+    statusText.innerText = "Status: Off";
+    btn.innerText = "Turn On";
+
+    bulb.classList.add("off");
+  }
+});
