17df8fc5114151c1bfa801a97a3d065e24b5b5e9
Nidhibhide <nidhibhide144@gmail.com>
2025-03-14 22:31:07 +0530

    JS DOM Challenge 9 completed

--- JS DOM Challenges/DOM CHallenges[6-10]/challenge-9/app.js
+++ JS DOM Challenges/DOM CHallenges[6-10]/challenge-9/app.js
@@ -1,3 +1,10 @@
-/**
- * Write your challenge solution here
- */
+const togglebtn=document.querySelector(".toggle-btn");
+const panel=document.querySelector(".panel")
+const closebtn=document.querySelector(".close-btn");
+togglebtn.addEventListener("click",()=>{
+panel.classList.add("active");
+})
+
+closebtn.addEventListener("click",()=>{
+    panel.classList.remove("active");
+})
